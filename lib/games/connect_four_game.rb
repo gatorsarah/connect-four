00306689acd9168baf2e97cda01d72482a89273a
Sarah Johnson <Sarah.Johnson@careerbuilder.com>
2017-01-31 23:07:56 -0500

    Adding a check for the user winning the game

--- lib/games/connect_four_game.rb
+++ lib/games/connect_four_game.rb
@@ -16,23 +16,73 @@ class ConnectFourGame
   def make_computers_move
   end
 
+  def did_user_win?
+    is_there_a_connect_four? 'computer'
+  end
+
   private
 
   def should_block?
   end
 
-  def on_a_streak?
-  end
+  def streak_count(column, row, direction, owner)
+ 
+    if is_row_valid?(column, row) && is_column_valid?(column) 
 
+      if @game_board[column][row].user = owner
+
+        next_row = row if direction == 'horizontal'
+        next_row = row + 1 if ['vertical', 'diagonal up'].include? direction
+        next_row = row -1 if direction == 'diagonal down'
+
+        next_column = column if direction == 'vertical'
+        next_column = column + 1 if ['horizontal', 'diagonal up'].include? direction
+        next_column = column - 1 if direction == 'diagonal down' 
+        
+        current_count = streak_count(next_column, next_row, direction, owner)
+        current_count = 1 if current_count.nil?
+        current_count += 1
+
+        puts "on the way back: #{current_count}"
+      end
+    end
+    current_count ||= 1
+  end
+  
   def take_slot(column, owner)
      @game_board[column] << Slot.new(owner)
-   end
+  end
 
   def is_column_full?(column)
     return false if @game_board[column].nil?
     @game_board[column].count >= ENV['MAX_ROWS'].to_i
   end
 
-  def is_there_a_connect_four?
+  def is_there_a_connect_four?(owner)
+    match_count = 0
+    @game_board.each do |column|
+      column.each do |row|
+        if owner = row.user
+          current_row = column.index(row)
+          current_column = @game_board.index(column)
+          vertical_count = streak_count(current_column, current_row + 1, "vertical", owner)
+          horizontal_count = streak_count(current_column + 1, current_row, "horizontal", owner)
+          up_count = streak_count(current_column + 1, current_row + 1, "diagonal up", owner)
+          down_count = streak_count(current_column - 1, current_row - 1, "diagonal down", owner)
+          match_count = [match_count, vertical_count, horizontal_count, up_count, down_count].max
+          puts "Found #{match_count}"
+        end
+      end
+    end
+    puts "final_match_count #{match_count}"
+    match_count
+  end
+
+  def is_row_valid?(column, row)
+    row >= 0 && row < ENV['MAX_ROWS'].to_i && row < @game_board[column].count
+  end
+
+  def is_column_valid?(column)
+    column >= 0 && column < ENV['MAX_COLUMNS'].to_i
   end
 end
--- spec/games/connect_four_game_spec.rb
+++ spec/games/connect_four_game_spec.rb
@@ -11,6 +11,27 @@ describe ConnectFourGame do
   it 'should take a new slot' do
     game = ConnectFourGame.new
     game.record_users_move(3)
-   end
+
+    expect(game.game_board[3].count).to eq 1
+    expect(game.game_board[0].count).to eq 0
+  end
+
+  it 'should find a winning horizontal row' do
+    game = ConnectFourGame.new
+    puts game.inspect
+    
+    game.game_board[1] << Slot.new('human')
+    game.game_board[1] << Slot.new('computer')
+    game.game_board[2] << Slot.new('computer')
+    game.game_board[2] << Slot.new('computer')
+    game.game_board[3] << Slot.new('human')
+    game.game_board[3] << Slot.new('computer')
+    game.game_board[4] << Slot.new('human')
+    game.game_board[4] << Slot.new('computer')
+
+    game.did_user_win?
+    
+    puts game.inspect
+  end
   
 end
