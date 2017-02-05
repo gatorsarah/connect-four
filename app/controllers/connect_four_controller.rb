f2dd6d709198ee2d25937f4c6617e5bb7338ea67
Sarah Johnson <Sarah.Johnson@careerbuilder.com>
2017-02-04 20:45:11 -0500

    new game view

--- app/controllers/connect_four_controller.rb
+++ app/controllers/connect_four_controller.rb
@@ -1,28 +1,38 @@
 class ConnectFourController < ApplicationController
-  
+
   def new_game
     @game = Games::ConnectFourGame.new
 
+    session[:game_board] = @game.game_board
     #redirect_to :computers_move_path new
   end
 
-  def computers_move
+  def make_computers_move
+    @game = Games::ConnectFourGame.new
+
+    @game.game_board = session[:game_board]
     @game.make_computers_move
 
-    #render board
-    
-    did_user_win? 'computer'
+        
+    @game.did_user_win? 'computer'
+
+    session[:game_board] = @game.game_board
     #if so, then popup contrats message 
   end
 
   def users_move
+
+    @game = Games::ConnectFourGame.new
+    @game.game_board = session[:game_board]
+    
     @game.record_users_move params[:column]
 
     #render choice
     
-    if did_user_win? 'human'
+    if @game.did_user_win? 'human'
     #if so then popup contrats message
     else
+      session[:game_board] = @game.game_board
       render computers_move_path
     end
   end
