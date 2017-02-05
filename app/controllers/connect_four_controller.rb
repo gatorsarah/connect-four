7cfcb15af448539ee1ddd7b67171ed0a4b70ce3f
Sarah Johnson <Sarah.Johnson@careerbuilder.com>
2017-02-05 08:57:32 -0500

    adding saving functionality

--- app/controllers/connect_four_controller.rb
+++ app/controllers/connect_four_controller.rb
@@ -40,4 +40,40 @@ class ConnectFourController < ApplicationController
       end
     end
   end
+
+  def list_games
+  end
+
+  def load_game
+  end
+
+  def save_game
+
+    game_board = session[:game_board]
+    
+    game_to_save = ConnectFourGame.create do |game|
+      game.column0 = Array.new
+      game.column1 = Array.new
+      game.column2 = Array.new
+      game.column3 = Array.new
+      game.column4 = Array.new
+      game.column5 = Array.new
+      game.column6 = Array.new      
+      
+      game.save_time = DateTime.now
+    end
+
+      game_board[0].each { |item| game_to_save.column0 << item["user"]} 
+      game_board[1].each { |item| game_to_save.column1 << item["user"]}
+      game_board[2].each { |item| game_to_save.column2 << item["user"]}
+      game_board[3].each { |item| game_to_save.column3 << item["user"]}
+      game_board[4].each { |item| game_to_save.column4 << item["user"]}
+      game_board[5].each { |item| game_to_save.column5 << item["user"]}
+      game_board[6].each { |item| game_to_save.column6 << item["user"]}
+      
+  
+    game_to_save.save!
+
+    redirect_to new_game_path, :notice => "Game saved! Starting new game!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   match 'connect_four/new' => 'connect_four#new_game', :via => [:get], :as => :new_game
   match 'connect_four/play/' => 'connect_four#make_computers_move', :via => [:get], :as => :make_computers_move
   match 'connect_four/play/:column' => 'connect_four#users_move', :via => [:get, :post], :as => :users_move
+
+  match 'connect_four/list' => 'connect_four#list_games', :via => [:get], :as => :list_games
+  match 'connect_four/load/:game' => 'connect_four#load_game', :via => [:get], :as => :load_game
+  match 'connect_four/save' => 'connect_four#save_game', :via => [:get], :as => :save_game
   
 end
